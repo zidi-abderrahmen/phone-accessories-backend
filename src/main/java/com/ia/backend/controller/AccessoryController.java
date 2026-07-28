7f6c7d095f2d43276dfe2966ba62d9ced72f948e
zidiabderrahmen <rhoumazaza@gmail.com>
2026-07-28 21:19:06 +0100

    feat: added AccessoryController for the simple CRUD and test the endpoints with the Swagger

---
+++ src/main/java/com/ia/backend/controller/AccessoryController.java
@@ -0,0 +1,46 @@
+package com.ia.backend.controller;
+
+import com.ia.backend.dto.accessory.AccessoryRequest;
+import com.ia.backend.dto.accessory.AccessoryResponse;
+import com.ia.backend.service.AccessoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/accessories") // Don't add '/api' because it's already added in the application.properties
+@RequiredArgsConstructor
+public class AccessoryController {
+
+    private final AccessoryService accessoryService;
+
+    @GetMapping
+    public ResponseEntity<Page<AccessoryResponse>> getAllAccessories(Pageable pageable) {
+        return ResponseEntity.ok(accessoryService.getAllAccessories(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccessoryResponse> getAccessoryById(@PathVariable Long id) {
+        return ResponseEntity.ok(accessoryService.getAccessoryById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AccessoryResponse> createAccessory(@Valid @RequestBody AccessoryRequest accessoryRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(accessoryService.createAccessory(accessoryRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AccessoryResponse> updateAccessory(@PathVariable Long id, @Valid @RequestBody AccessoryRequest accessoryRequest) {
+        return ResponseEntity.ok(accessoryService.updateAccessory(id, accessoryRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccessory(@PathVariable Long id) {
+        accessoryService.deleteAccessory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
