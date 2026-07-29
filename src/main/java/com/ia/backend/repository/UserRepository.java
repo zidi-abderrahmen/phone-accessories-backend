1a2bc0accf44b54726ba3e2570facd0040dab5f5
zidiabderrahmen <rhoumazaza@gmail.com>
2026-07-29 22:52:43 +0100

    feat: added UserREpository, RefreshTokenRepository, UserRepository and EmailVerificationRepository

---
+++ src/main/java/com/ia/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ia.backend.repository;
+
+import com.ia.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
