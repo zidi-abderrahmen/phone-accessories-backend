9d8209168326f3b46193599f48806976052d8cc6
zidiabderrahmen <rhoumazaza@gmail.com>
2026-07-30 21:50:53 +0100

    feat: updated PasswordEncoderConfig to use DelegatingPasswordEncoder with BCryptPasswordEncoder

--- src/main/java/com/ia/backend/configs/PasswordEncoderConfig.java
+++ src/main/java/com/ia/backend/configs/PasswordEncoderConfig.java
@@ -3,13 +3,21 @@ package com.ia.backend.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class PasswordEncoderConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
+        String currentId = "bcrypt";
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put(currentId, new BCryptPasswordEncoder(12));
+
+        return new DelegatingPasswordEncoder(currentId, encoders);
     }
 }
