c60a1adc6394ed771d941cd3503adf1839dfe0a2
zidiabderrahmen <rhoumazaza@gmail.com>
2026-07-30 16:48:19 +0100

    feat: added the auth edpoints

---
+++ src/main/java/com/ia/backend/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.ia.backend.controller;
+
+import com.ia.backend.dto.reftoken.RefreshTokenRequest;
+import com.ia.backend.dto.reftoken.RefreshTokenResponse;
+import com.ia.backend.dto.user.UserLoginRequest;
+import com.ia.backend.dto.user.UserLoginResponse;
+import com.ia.backend.dto.user.UserRegisterRequest;
+import com.ia.backend.dto.user.UserResponse;
+import com.ia.backend.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody UserRegisterRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserLoginResponse> login(@Valid @RequestBody UserLoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<RefreshTokenResponse> refreshToken(@Valid @RequestBody RefreshTokenRequest request) {
+        return ResponseEntity.ok(authService.refreshToken(request));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@Valid @RequestBody RefreshTokenRequest request) {
+        authService.logout(request);
+        return ResponseEntity.noContent().build();
+    }
+}
