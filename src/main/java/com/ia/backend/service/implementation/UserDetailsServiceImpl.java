21da300cb3d5a1fe17a4d57938a36dcbe9345f8e
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-03 12:16:28 +0100

    fix: added UserPrincipal class for UserDetailsServiceImpl to return user details and authorities

--- src/main/java/com/ia/backend/service/implementation/UserDetailsServiceImpl.java
+++ src/main/java/com/ia/backend/service/implementation/UserDetailsServiceImpl.java
@@ -2,15 +2,13 @@ package com.ia.backend.service.implementation;
 
 import com.ia.backend.entity.User;
 import com.ia.backend.repository.UserRepository;
+import com.ia.backend.util.UserPrincipal;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,19 +22,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getEmail())
-                .password(user.getPassword())
-                .disabled(!user.isEnabled())
-
-                .authorities(
-                        user.getRoles().stream()
-                                .map(role -> "ROLE_" + role.getName())
-
-                                .map(SimpleGrantedAuthority::new)
-
-                                .collect(Collectors.toList())
-                )
-                .build();
+        return new UserPrincipal(user);
     }
 }
