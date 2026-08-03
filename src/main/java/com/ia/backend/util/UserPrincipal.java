f29491d40158d48e5176aa8de03e531b26914c3f
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-03 12:14:41 +0100

    feat: created UserPrincipal class to handle user details and authorities

---
+++ src/main/java/com/ia/backend/util/UserPrincipal.java
@@ -0,0 +1,43 @@
+package com.ia.backend.util;
+
+import com.ia.backend.entity.User;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public record UserPrincipal(User user) implements UserDetails {
+
+    @Override
+    @NonNull
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    @NonNull
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !user.isDeleted();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
