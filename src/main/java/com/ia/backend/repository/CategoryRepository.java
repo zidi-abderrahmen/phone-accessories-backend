93c5c8623c71a98bfebc4936f41d04c6044834b3
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-05 22:51:06 +0100

    feat: updated category repository to use findByNameIgnoreCase and existsByNameIgnoreCase methods

--- src/main/java/com/ia/backend/repository/CategoryRepository.java
+++ src/main/java/com/ia/backend/repository/CategoryRepository.java
@@ -4,9 +4,9 @@ import com.ia.backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.Set;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    Optional<Set<Category>> findByName(String name);
+    Optional<Category> findByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCase(String name);
 }
