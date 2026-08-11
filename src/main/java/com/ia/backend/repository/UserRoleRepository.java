d127fb6ef9ea275960ecfe06b10537d0f17ac915
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-11 16:59:32 +0100

    feat: updated UserRoleRepository by adding methods for finding by name and find all by deleted

--- src/main/java/com/ia/backend/repository/UserRoleRepository.java
+++ src/main/java/com/ia/backend/repository/UserRoleRepository.java
@@ -3,9 +3,16 @@ package com.ia.backend.repository;
 import com.ia.backend.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
     Optional<UserRole> findByName(String name);
+
+    List<UserRole> findAllByDeleted(boolean deleted);
+
+    boolean existsByName(String name);
+
+    boolean existsByNameIgnoreCase(String name);
 }
