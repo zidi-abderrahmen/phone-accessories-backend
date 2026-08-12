b1d2217e9eacc825f1408b4fa4db3b5d95dd3497
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-12 23:40:54 +0100

    feat: added JpaSpecificationExecutor interface to AccessoryRepository

--- src/main/java/com/ia/backend/repository/AccessoryRepository.java
+++ src/main/java/com/ia/backend/repository/AccessoryRepository.java
@@ -4,8 +4,9 @@ import com.ia.backend.entity.Accessory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AccessoryRepository extends JpaRepository<Accessory, Long> {
+public interface AccessoryRepository extends JpaRepository<Accessory, Long>, JpaSpecificationExecutor<Accessory> {
 
     boolean existsByProductCode(String productCode);
 
