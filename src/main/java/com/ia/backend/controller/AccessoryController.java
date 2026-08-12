124010db6061e0110b50c30cd2d1a7c46ec51123
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-12 23:42:25 +0100

    feat: added search endpoint for accessories with JPA specification

--- src/main/java/com/ia/backend/controller/AccessoryController.java
+++ src/main/java/com/ia/backend/controller/AccessoryController.java
@@ -14,8 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-
 @RestController
 @RequestMapping("/accessories") // Don't add '/api' because it's already added in the application.properties
 @RequiredArgsConstructor
