12089cf7848658bbd2c108c1c292818e83d9e472
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-18 00:36:56 +0100

    feat: renamed the AuthMaper to UserMapper

--- src/main/java/com/ia/backend/mapper/UserMapper.java
+++ src/main/java/com/ia/backend/mapper/UserMapper.java
@@ -2,20 +2,25 @@ package com.ia.backend.mapper;
 
 import com.ia.backend.dto.user.UserRegisterRequest;
 import com.ia.backend.dto.user.UserResponse;
+import com.ia.backend.dto.user.profile.UpdateProfileRequest;
 import com.ia.backend.entity.User;
 import com.ia.backend.entity.UserRole;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
+import org.mapstruct.*;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
-public interface AuthMapper {
+public interface UserMapper {
 
     @Mapping(target = "roles", source = "roles", qualifiedByName = "mapRoles")
     UserResponse toUserResponse(User user);
+
+    @Mapping(target = "roles", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "email", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateUser(UpdateProfileRequest request, @MappingTarget User user);
 
     @Mapping(target = "password", ignore = true)
     @Mapping(target = "roles", ignore = true)
