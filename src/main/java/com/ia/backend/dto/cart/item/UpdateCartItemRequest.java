831f33f52edc59420e1c780e8b3b123e524e2177
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-20 01:54:52 +0100

    feat: removed cart item ID field from UpdateCartItemRequest

--- src/main/java/com/ia/backend/dto/cart/item/UpdateCartItemRequest.java
+++ src/main/java/com/ia/backend/dto/cart/item/UpdateCartItemRequest.java
@@ -5,10 +5,6 @@ import jakarta.validation.constraints.Positive;
 
 public record UpdateCartItemRequest(
 
-        @NotNull(message = "Cart item ID cannot be null.")
-        @Positive(message = "Cart item ID must be positive.")
-        Long cartItemId,
-
         @NotNull(message = "New quantity cannot be null.")
         @Positive(message = "New quantity must be positive.")
         int newQuantity
