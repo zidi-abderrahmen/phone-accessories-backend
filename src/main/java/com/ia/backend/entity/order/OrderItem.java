3d932515ef7a990cadce7df8d20308ffdc3e9b66
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-21 23:52:35 +0100

    feat: added @Builder annotation to OrderItem entity

--- src/main/java/com/ia/backend/entity/order/OrderItem.java
+++ src/main/java/com/ia/backend/entity/order/OrderItem.java
@@ -2,10 +2,7 @@ package com.ia.backend.entity.order;
 
 import com.ia.backend.entity.Accessory;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -20,6 +17,7 @@ import java.math.BigDecimal;
 )
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
+@Builder
 public class OrderItem {
 
     @Id
