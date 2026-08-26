c740f8cc1fdaefcc42ee0d4ec11b38c2743a53c0
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-26 22:53:52 +0100

    feat: added ReviewRepository interface with CRUD operations for reviews

--- src/main/java/com/ia/backend/repository/ReviewRepository.java
+++ src/main/java/com/ia/backend/repository/ReviewRepository.java
@@ -4,8 +4,6 @@ import com.ia.backend.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
