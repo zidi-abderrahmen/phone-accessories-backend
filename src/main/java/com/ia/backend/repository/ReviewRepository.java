f867057b5d2f3d2add2e4b5ab7ae391447e1b09a
zidiabderrahmen <rhoumazaza@gmail.com>
2026-08-26 22:51:28 +0100

    feat: added ReviewRepository interface with CRUD operations for reviews

---
+++ src/main/java/com/ia/backend/repository/ReviewRepository.java
@@ -0,0 +1,18 @@
+package com.ia.backend.repository;
+
+import com.ia.backend.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findAllByAccessory_Id(Long accessoryId, Pageable pageable);
+    List<Review> findAllByUser_IdAndAccessory_Id(String userId, Long accessoryId);
+    Optional<Review> findByUser_IdAndId(String userId, Long reviewId);
+    boolean existsByUser_IdAndAccessory_Id(String userId, Long accessoryId);
+    boolean existsByUser_IdAndId(String userId, Long reviewId);
+}
