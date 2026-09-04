6343e8bd9fdf5afbd687aeb44a777806a893df1c
Zidi Abderrahmen <zd.abderrahmen@gmail.com>
2026-09-04 22:48:32 +0100

    fix: update accessory repository to include entity graph for category retrieval

--- src/main/java/com/ia/backend/accessory/repository/AccessoryRepository.java
+++ src/main/java/com/ia/backend/accessory/repository/AccessoryRepository.java
@@ -3,6 +3,7 @@ package com.ia.backend.accessory.repository;
 import com.ia.backend.accessory.entity.Accessory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -10,5 +11,6 @@ public interface AccessoryRepository extends JpaRepository<Accessory, Long>, Jpa
 
     boolean existsByProductCode(String productCode);
 
+    @EntityGraph(attributePaths = "category")
     Page<Accessory> findAllByCategory_Id(Long categoryId, Pageable pageable);
 }
